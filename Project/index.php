63171d459ff66fced7024efaef277bb0af3b48f0
Simon Árpád <simonarpad2002@gmail.com>
2021-10-01 17:23:54 +0200

    Alap struktúra: Weboldal alapja
    Weboldal alapja  megírva az index.php-ban

---
+++ Project/index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<html lang="hu">
+    <head>
+        <title>Pizzazo</title>
+    </head>
+    <body>
+        <header><?php @include_once 'private/components/header.php'; ?></header>
+        <nav><?php require_once 'private/components/nav.php'; ?></nav>
+        <main></main>
+        <footer><?php @include_once 'private/components/footer.php'?></footer>
+    </body>
+</html>
