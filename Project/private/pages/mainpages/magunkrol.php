eb7d18529fef9470d315e751b76c695c3078c81e
D. Tóth Gyula Bátor <batordtoth@gmail.com>
2021-10-04 01:28:27 +0200

    A pizzériáról szóló oldal létre hozása
    A pizzéria oldal bemutatását biztosító oldal meg lett írva és fel lett töltve

---
+++ Project/private/pages/mainpages/magunkrol.php
@@ -0,0 +1,38 @@
+<div class="position-absolute top-50 start-50 translate-middle" style="width: 50%">
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    A konyháról
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    Jó felszerelt konyhánkban, igény szerinti, bőséges, egyedi pizzák készítése zajlik nap mint nap. A pizzák tésztái bármi féle gép nélkül hagyományosan kézi erővel készülnek.</div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Alapanyagok
+                </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    Alapanyagaink 30% külföldről, Olaszországból míg 70%-át hazai termelőktől szerezzük be.</div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    A mesterszakácsunk
+                </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    A szakácsunk még Olaszorszában, mesterektől leste el a pizzakészítés művészetét
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
