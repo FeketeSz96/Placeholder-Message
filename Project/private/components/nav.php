dd46fe9e65fabacf7edbbfeb02987e178a54224f
Szeifert Roland <szeifert.r0119@gmail.com>
2021-10-03 15:35:12 +0200

    Nav bár rész be-, kijelentkezéses funkció

--- Project/private/components/nav.php
+++ Project/private/components/nav.php
@@ -22,6 +22,18 @@
                     <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Profil
                     </a>
+                    <?php if (isset($_SESSION['user'])): ?>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href='?p=auth/profil'>Profil</a></li>
+                            <li><a class="dropdown-item" href='?p=auth/logout'>Kijelentkezés</a></li>
+                        </ul>
+                    <?php else: ?>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="?p=auth/register">Regisztráció</a></li>
+                            <li><a class="dropdown-item" href="?p=auth/login">Bejelentkezés</a></li>
+                        </ul>
+                    <?php endif; ?>
+                </li>
             </ul>
         </div>
     </div>
